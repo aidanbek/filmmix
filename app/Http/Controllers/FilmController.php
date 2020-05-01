272d52c73c2964788b9be0eddf699caa2ca81821
Айданбек Калымбеков <aidanbek.k@gmail.com>
2020-05-01 21:15:20 +0600

    add updating film data

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -13,7 +13,7 @@ class FilmController extends Controller
 {
     public function index()
     {
-        $films = Film::with('actors', 'directors')->orderBy('title')->orderBy('prod_year')->get();
+        $films = Film::with('actors', 'directors', 'genres')->orderBy('title')->orderBy('prod_year')->get();
         return view('film.index', compact('films'));
     }
 
@@ -49,22 +49,56 @@ class FilmController extends Controller
 
     public function show($id)
     {
+        $actors = Actor::orderBy('title')->get();
+        $directors = Director::orderBy('title')->get();
+        $genres = Genre::orderBy('title')->get();
         $film = Film::with('actors', 'directors', 'genres')->where('id', $id)->first();
-        return view('film.show', compact('film'));
+        return view('film.show', compact('film', 'actors', 'directors', 'genres'));
     }
 
     public function edit($id)
     {
-        //
+
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'prod_year' => 'required|integer|min:1800|max:3000',
+            'actors' => 'nullable|array|exists:users,id',
+            'directors' => 'nullable|array|exists:users,id',
+            'genres' => 'nullable|array|exists:genres,id'
+        ]);
+
+        $film = Film::find($id)->first();
+        $film->title = $request->title;
+        $film->prod_year = $request->prod_year;
+        $film->save();
+
+        $existedActors = $film->actors->pluck('id')->toArray();
+        $actorsToAdd = array_diff($request->actors, $existedActors);
+        $actorsToRemove = array_diff($existedActors, $request->actors);
+        $film->actors()->attach($actorsToAdd);
+        $film->actors()->detach($actorsToRemove);
+
+        $existedDirectors = $film->directors->pluck('id')->toArray();
+        $directorsToAdd = array_diff($request->directors, $existedDirectors);
+        $directorsToRemove = array_diff($existedDirectors, $request->directors);
+        $film->directors()->attach($directorsToAdd);
+        $film->directors()->detach($directorsToRemove);
+
+        $existedGenres = $film->genres->pluck('id')->toArray();
+        $genresToAdd = array_diff($request->genres, $existedGenres);
+        $genresToRemove = array_diff($existedGenres, $request->genres);
+        $film->genres()->attach($genresToAdd);
+        $film->genres()->detach($genresToRemove);
+
+        return back();
     }
 
     public function destroy($id)
     {
-        //
+
     }
 }
--- resources/views/film/show.blade.php
+++ resources/views/film/show.blade.php
@@ -1,1 +1,133 @@
-<?php
+@extends('layouts.default')
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="jumbotron">
+                <h1 class="display-4">{{$film->title}}
+                    @if(!is_null($film->prod_year))
+                        <span class="badge badge-light">{{$film->prod_year}}</span>
+                    @endif
+                </h1>
+                @foreach($film->genres as $genre)
+                    <a href="{{route('genres.show', $genre->id)}}" class="badge badge-primary">{{$genre->title}}</a>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <b>Режиссеры:</b>
+            @foreach($film->directors as $director)
+                @include('components.director_link', compact('director'))
+            @endforeach
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <b>Актеры:</b>
+            @foreach($film->actors as $actor)
+                @include('components.actor_link', compact('actor'))
+            @endforeach
+        </div>
+    </div>
+    <div class="row my-3">
+        <div class="col-sm-12">
+            <button type="button" class="btn btn-outline-primary btn-sm" data-toggle="modal" data-target="#edit_film">
+                Редактировать
+            </button>
+            <button type="button" class="btn btn-outline-danger btn-sm">Удалить</button>
+        </div>
+    </div>
+
+    <div class="modal fade" id="edit_film" tabindex="-1" role="dialog" aria-labelledby="edit_film_title"
+         aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="edit_film_title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{route('films.update', $film->id)}}" method="POST">
+                        @method('PATCH')
+                        @csrf
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-9">
+                                    <label for="title">Название</label>
+                                    <input type="text" class="form-control" name="title" id="title"
+                                           value="{{$film->title}}">
+                                </div>
+                                <div class="col-sm-3">
+                                    <label for="prod_year">Год</label>
+                                    <input type="number" class="form-control" name="prod_year" id="prod_year"
+                                           value="{{$film->prod_year}}">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <label for="genres">Жанры</label>
+                                    <select class="selectpicker form-control" name="genres[]" multiple
+                                            data-live-search="true">
+                                        @php $currentGenres = $film->genres->pluck('id')->toArray(); @endphp
+                                        @foreach($genres as $genre)
+                                            @if(in_array($genre->id, $currentGenres))
+                                                <option selected value="{{$genre->id}}">{{$genre->title}}</option>
+                                            @else
+                                                <option value="{{$genre->id}}">{{$genre->title}}</option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <label for="genres">Режиссеры</label>
+                                    <select class="selectpicker form-control" name="directors[]" multiple
+                                            data-live-search="true">
+                                        @php $currentDirectors = $film->directors->pluck('id')->toArray(); @endphp
+                                        @foreach($directors as $director)
+                                            @if(in_array($director->id, $currentDirectors))
+                                                <option selected value="{{$director->id}}">{{$director->title}}</option>
+                                            @else
+                                                <option value="{{$director->id}}">{{$director->title}}</option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <label for="genres">Актеры</label>
+                                    <select class="selectpicker form-control" name="actors[]" multiple
+                                            data-live-search="true">
+                                        @php $currentActors = $film->actors->pluck('id')->toArray(); @endphp
+                                        @foreach($actors as $actor)
+                                            @if(in_array($actor->id, $currentActors))
+                                                <option selected value="{{$actor->id}}">{{$actor->title}}</option>
+                                            @else
+                                                <option value="{{$actor->id}}">{{$actor->title}}</option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Сохранить</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
