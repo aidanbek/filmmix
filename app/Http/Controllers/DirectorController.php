047b7197500a816cdecdcab1aaa3c9304e2fa2aa
Айданбек Калымбеков <aidanbek.k@gmail.com>
2020-05-01 22:48:37 +0600

    add show page for director; add deleting director

--- app/Http/Controllers/DirectorController.php
+++ app/Http/Controllers/DirectorController.php
@@ -35,21 +35,26 @@ class DirectorController extends Controller
 
     public function show($id)
     {
-        //
+        $director = Director::with('films', 'films.actors', 'films.genres', 'films.directors')
+            ->where('id', $id)
+            ->first();
+        return view('director.show', compact('director'));
     }
 
     public function edit($id)
     {
-        //
+
     }
 
     public function update(Request $request, $id)
     {
-        //
+
     }
 
     public function destroy($id)
     {
-        //
+        $director = Director::where('id', $id)->first();
+        $director->delete();
+        return redirect(route('directors.index'));
     }
 }
--- resources/views/director/show.blade.php
+++ resources/views/director/show.blade.php
@@ -1,1 +1,47 @@
-<?php
+@extends('layouts.default')
+
+@section('content')
+    <div class="row mb-3">
+        <div class="col-sm-12">
+            <h1>{{$director->title}}</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            @include('components.film_table', ['films' => $director->films])
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col-sm-12">
+            @if($director->films->count() === 0)
+                <button type="button"
+                        class="btn btn-outline-danger btn-sm"
+                        data-toggle="modal"
+                        data-target="#delete_director">Удалить
+                </button>
+            @endif
+        </div>
+    </div>
+
+    <div class="modal fade" id="delete_director" tabindex="-1" role="dialog" aria-labelledby="delete_director_title"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="delete_director_title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{route('directors.destroy', $director->id)}}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                        <button type="submit" class="btn btn-danger">Удалить</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
