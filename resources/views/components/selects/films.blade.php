9aef778324dab0291b1963fafed879816771d8b4
Aidanbek Kalymbekov <aidanbek@protonmail.com>
2020-07-21 22:03:17 +0600

    add selecting films on selector

--- resources/views/components/selects/films.blade.php
+++ resources/views/components/selects/films.blade.php
@@ -1,6 +1,16 @@
 <label for="films">Фильмы</label>
 <select class="selectpicker form-control" name="films[]" multiple data-live-search="true">
-    @foreach($films as $film)
-        <option value="{{$film->id}}">{{$film->title}}</option>
-    @endforeach
+    @if(isset($currentFilms))
+        @foreach($films as $film)
+            @if(in_array($film->id, $currentFilms))
+                <option selected value="{{$film->id}}">{{$film->title}}</option>
+            @else
+                <option value="{{$film->id}}">{{$film->title}}</option>
+            @endif
+        @endforeach
+    @else
+        @foreach($films as $film)
+            <option value="{{$film->id}}">{{$film->title}}</option>
+        @endforeach
+    @endif
 </select>
